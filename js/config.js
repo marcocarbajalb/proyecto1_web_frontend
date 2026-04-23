34c615ff307aa96f38b0eff68eecb3efffd4f3f4
Marco Carbajal <car23025@uvg.edu.gt>
2026-04-23 08:37:22 -0600

    Apuntar frontend al backend deployado

--- js/config.js
+++ js/config.js
@@ -1,3 +1,5 @@
 const CONFIG = {
-    API_BASE_URL: 'http://localhost:8080'
+    API_BASE_URL: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+        ? 'http://localhost:8080'
+        : 'https://proyecto1-web-backend.fly.dev'
 };
